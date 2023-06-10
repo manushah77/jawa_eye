9fb5644a655222c88785eda04cfea75b56e151cf
Junaid <junaidkhokharci@gmail.com>
2023-06-11 02:18:47 +0500

    adding registration and login

--- lib/Screens/LoginScreen/login_screen.dart
+++ lib/Screens/LoginScreen/login_screen.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:jawa_eye/Constant/color.dart';
-import 'package:jawa_eye/Screens/BottomNavigationBar/bottom_navigation_bar_screen.dart';
 import 'package:jawa_eye/Screens/SignupScreen/signup_screen.dart';
 import 'package:jawa_eye/Screens/widget/custom_button.dart';
+import 'package:jawa_eye/providers/api_client.dart';
+import 'package:jawa_eye/providers/auth_provider.dart';
+import 'package:jawa_eye/providers/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../main.dart';
+import '../BottomNavigationBar/bottom_navigation_bar_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -20,6 +25,37 @@ class _LoginScreenState extends State<LoginScreen> {
   bool ispasswordvisible = true;
   bool _isChecked = false;
   final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    requestPermission();
+  }
+
+  void requestPermission() async {
+    FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+    NotificationSettings notificationSettings = await firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: true,
+      provisional: false,
+      sound: true,
+    );
+
+    if (notificationSettings.authorizationStatus == AuthorizationStatus.authorized) {
+      print("Granted");
+    } else {
+      print('faile');
+    }
+
+    await firebaseMessaging.getToken().then((value) {
+      deviceToken = value;
+      print('Token: $value');
+    });
+    getFirebaseMessaging();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +80,7 @@ class _LoginScreenState extends State<LoginScreen> {
             Container(
               height: 440.h,
               width: double.infinity,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.black54,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30),
@@ -56,7 +92,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   SizedBox(
                     height: 30.h,
                   ),
-                  Text(
+                  const Text(
                     'Email Login',
                     style: TextStyle(fontSize: 16, color: Colors.white),
                   ),
@@ -66,7 +102,8 @@ class _LoginScreenState extends State<LoginScreen> {
                   SizedBox(
                     width: 280.w,
                     child: TextFormField(
-                      decoration: InputDecoration(
+                      controller: emailC,
+                      decoration: const InputDecoration(
                         border: UnderlineInputBorder(
                             borderSide: BorderSide(
                           color: Colors.white,
@@ -87,7 +124,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           color: Colors.white38,
                         ),
                       ),
-                      style: TextStyle(
+                      style: const TextStyle(
                         color: Colors.white,
                       ),
                       cursorColor: Colors.white,
@@ -99,6 +136,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   SizedBox(
                     width: 280.w,
                     child: TextFormField(
+                      controller: passwordC,
                       decoration: InputDecoration(
                         border: UnderlineInputBorder(
                             borderSide: BorderSide(
@@ -110,13 +148,13 @@ class _LoginScreenState extends State<LoginScreen> {
                           color: Colors.white,
                           width: 1.w,
                         )),
-                        enabledBorder: UnderlineInputBorder(
+                        enabledBorder: const UnderlineInputBorder(
                             borderSide: BorderSide(
                           color: Colors.white,
                           width: 1,
                         )),
-                        hintText: 'Enter your 6-15 digit password',
-                        hintStyle: TextStyle(
+                        hintText: 'Enter your password',
+                        hintStyle: const TextStyle(
                           color: Colors.white38,
                         ),
                         suffixIcon: IconButton(
@@ -127,14 +165,12 @@ class _LoginScreenState extends State<LoginScreen> {
                           },
                           icon: Icon(
                             size: 20,
-                            ispasswordvisible
-                                ? Icons.visibility_off
-                                : Icons.visibility,
+                            ispasswordvisible ? Icons.visibility_off : Icons.visibility,
                             color: Colors.white,
                           ),
                         ),
                       ),
-                      style: TextStyle(
+                      style: const TextStyle(
                         color: Colors.white,
                       ),
                       cursorColor: Colors.white,
@@ -154,8 +190,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           width: 24.0,
                           child: Theme(
                             data: ThemeData(
-                              unselectedWidgetColor:
-                                  Colors.white60, // Your color
+                              unselectedWidgetColor: Colors.white60, // Your color
                             ),
                             child: Checkbox(
                                 shape: RoundedRectangleBorder(
@@ -176,7 +211,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           fontSize: 14.sp,
                         ),
                       ),
-                      SizedBox(width: 70.0),
+                      const SizedBox(width: 70.0),
                       Align(
                         alignment: Alignment.bottomRight,
                         child: TextButton(
@@ -201,15 +236,24 @@ class _LoginScreenState extends State<LoginScreen> {
                   SizedBox(
                     height: 40.h,
                   ),
-                  CustomButton(txt: 'Login', onTap: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => BottomNavBar(),
-                      ),
-                    );
-                  },),
-
+                  CustomButton(
+                    txt: 'Login',
+                    onTap: () {
+                      locator
+                          .get<AuthProvider>()
+                          .authenticate(email: emailC.text, password: passwordC.text, deviceToken: deviceToken ?? '')
+                          .then((value) {
+                        if (value.userId != null) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => BottomNavBar(),
+                            ),
+                          );
+                        }
+                      });
+                    },
+                  ),
                   SizedBox(
                     height: 20.h,
                   ),
@@ -232,7 +276,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => SignupScreen(),
+                              builder: (context) => const SignupScreen(),
                             ),
                           );
                         },
@@ -272,16 +316,11 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void _loadUserEmailPassword() async {
-    print("Load Email");
     try {
       SharedPreferences _prefs = await SharedPreferences.getInstance();
       var _email = _prefs.getString("email") ?? "";
       var _password = _prefs.getString("password") ?? "";
       var _remeberMe = _prefs.getBool("remember_me") ?? false;
-
-      print(_remeberMe);
-      print(_email);
-      print(_password);
       if (_remeberMe) {
         setState(() {
           _isChecked = true;
@@ -289,8 +328,6 @@ class _LoginScreenState extends State<LoginScreen> {
         emailC.text = _email;
         passwordC.text = _password;
       }
-    } catch (e) {
-      print(e);
-    }
+    } catch (e) {}
   }
 }
